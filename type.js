3db46a6b73be84201a84c5269a7c5709590653b4
Mohammad <mohammadpoorjamal@gmail.com>
2022-08-07 19:37:37 +0430

    2.4 Example of types

---
+++ type.js
@@ -0,0 +1,16 @@
+//  with errors
+const today = new Date()
+today.getDate();
+today.kasdhk()
+
+const person = {
+  age: 20
+}
+const age = person.age;
+const shoes = person.shoes;
+
+class Color {
+
+}
+const red = new Color()
+const rgb = red.rgb;
