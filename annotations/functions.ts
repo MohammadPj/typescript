9a030c8294e05e785b9b1e5d834d757b9e8c6038
Mohammad <mohammadpoorjamal@gmail.com>
2022-08-08 15:12:01 +0430

    4.4 void and never

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -15,3 +15,13 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
     return a * b
 }
+
+//  برای توابعی که Return ندارن
+const logger = (message: string): void => {
+    console.log(message)
+}
+
+//  برای توابعی که هیچ وقت کامل اجرا نمیشن
+const throwError = (message: string): never => {
+    throw new Error(message)
+}
