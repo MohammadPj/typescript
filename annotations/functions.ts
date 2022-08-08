df85a151a51233086db829d6dc011544ad815a7e
Mohammad <mohammadpoorjamal@gmail.com>
2022-08-08 15:20:41 +0430

    4.5 destructuring with annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -5,7 +5,7 @@ const add = (a: number, b: number) => {
 
 //  correct
 const abstract = (a: number, b: number): number => {
-   return  a - b
+    return a - b
 }
 
 function divide(a: number, b: number): number {
@@ -25,3 +25,14 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
     throw new Error(message)
 }
+
+const todaysWeather = {
+    date: new Date(),
+    weather: "sunny"
+}
+
+const logWeather = ({date, weather}: { date: Date, weather: string }): void => {
+    console.log(date)
+    console.log(weather)
+}
+logWeather(todaysWeather)
