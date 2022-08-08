e233a718a879d9fd2b9172a8f2cefdc7ac5500b4
Mohammad <mohammadpoorjamal@gmail.com>
2022-08-08 16:38:15 +0430

    5.3 multiple types

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -2,3 +2,18 @@ const carMakers: string[] = ["ford", "toyota", "jac"];
 
 //  2 dimensional arrays
 const carModels: string[][] = [["f16"], ["carolla", "highlux"], ["s3", "s5"]];
+
+//  Help with inference when extracting values
+const car = carMakers[0]
+const myCar = carMakers.pop()
+
+//  Prevent incompatible values
+carMakers.push(1)
+
+//  Help with map
+carMakers.map((car: string): string => {
+    return car
+})
+
+//  Flexible types
+const importantDates: (string | Date)[] = [new Date(), "2022-8-8"]
