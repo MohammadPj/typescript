71a492d03434d5f5ffbb35e613b348b22df5eef3
Mohammad <mohammadpoorjamal@gmail.com>
2022-08-26 14:57:55 +0430

    7.4 function inside interface

--- annotations/interfacees.ts
+++ annotations/interfacees.ts
@@ -1,13 +1,17 @@
 interface Vehicle {
-    name: string,
-    year: number,
-    broken: boolean
+  name: string;
+  year: number;
+  broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: "civic",
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name ${this.name}`;
+  },
 };
 
 //  long annotation problem XXX
@@ -23,10 +27,7 @@ const oldCivic = {
 
 //  fix long annotation with interface
 const printVehicle = (vehicle: Vehicle) => {
-    console.log("Name", vehicle.name);
-    console.log("Year", vehicle.year);
-    console.log("broken", vehicle.broken);
+    console.log(vehicle.summary())
 };
 
 printVehicle(oldCivic);
-
