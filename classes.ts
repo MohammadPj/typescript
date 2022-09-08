8bf46c00a86cf495e79d66603a354187699ca9cb
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-08 13:54:35 +0430

    8.3 private and protected methods in classes

--- classes.ts
+++ classes.ts
@@ -1,20 +1,29 @@
 class Vehicle {
-    drive(): void {
-        console.log("it is driving")
-    }
-
-    honk(): void {
+    //  متد های private رو حتی از children ها هم نمیشه صدا زد
+    private honk(): void {
         console.log("Beep Beep")
+    }
+    //  متد های protected رو از children ها میشه صدا زد اما خارج از children نمیشه
+    protected break() {
+        console.log("start break")
     }
 }
 
 class Car extends Vehicle {
-//  override methode drive
-    drive():void {
+    // متد های private فقط از داخل همین کلاس صدا زده میشن
+    //  متد های private فقط برای این استفاده میشن که سایر دولوپر ها به اشتباه این متد رو صدا نزنن و استفاده نادرست نکنن
+    //  در اصل نمی خوایم بقیه دولوپر ها این متد ها رو صدا بزنن
+    private drive():void {
         console.log("drive car")
     }
 
+    startDriving() {
+        this.drive()
+        this.honk()
+        this.break()
+    }
 }
 
 const car = new Car()
-car.drive()
+car.startDriving()
+car.break()
