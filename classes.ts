672e14165dca1cc0a372e18ee5541c36a40c38f8
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-08 14:26:51 +0430

    8.4 constructor

--- classes.ts
+++ classes.ts
@@ -1,29 +1,27 @@
 class Vehicle {
-    //  متد های private رو حتی از children ها هم نمیشه صدا زد
-    private honk(): void {
-        console.log("Beep Beep")
-    }
-    //  متد های protected رو از children ها میشه صدا زد اما خارج از children نمیشه
-    protected break() {
-        console.log("start break")
-    }
+  constructor(public color: string) {}
+
+  private honk(): void {
+    console.log("Beep Beep");
+  }
+  protected break() {
+    console.log("start break");
+  }
 }
 
 class Car extends Vehicle {
-    // متد های private فقط از داخل همین کلاس صدا زده میشن
-    //  متد های private فقط برای این استفاده میشن که سایر دولوپر ها به اشتباه این متد رو صدا نزنن و استفاده نادرست نکنن
-    //  در اصل نمی خوایم بقیه دولوپر ها این متد ها رو صدا بزنن
     private drive():void {
         console.log("drive car")
     }
 
     startDriving() {
         this.drive()
-        this.honk()
         this.break()
     }
 }
 
-const car = new Car()
+const vehicle = new Vehicle("blue")
+console.log(vehicle.color)
+
+const car = new Car("red")
 car.startDriving()
-car.break()
