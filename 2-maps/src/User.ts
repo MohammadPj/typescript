f77909ebb8d805a9b3678441cfd237e5bb4ef28f
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-09 16:51:32 +0430

    9.6 construct username and location

--- 2-maps/src/User.ts
+++ 2-maps/src/User.ts
@@ -1,1 +1,16 @@
-class User {}
+import { faker } from '@faker-js/faker';
+
+class User {
+    name: string;
+    loacation: {
+        lat: number;
+        lng: number
+    }
+    constructor() {
+        this.name = faker.name.firstName("male")
+        this.loacation = {
+            lat: +faker.address.latitude(),
+            lng: +faker.address.longitude()
+        }
+    }
+}
