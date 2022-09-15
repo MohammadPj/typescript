2da107fd9b094fec9b795c8106880bfb028e1d83
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-15 17:16:25 +0430

    9-10 finish map

--- 2-maps/src/index.ts
+++ 2-maps/src/index.ts
@@ -1,4 +1,25 @@
 import {User} from "./User";
+import {Company} from "./Company";
 
 const user = new User()
+const company = new Company()
+
 console.log("user", user)
+console.log("company", company)
+
+const companyLocation = company.location
+const userLocation = user.loacation
+
+// @ts-ignore
+var map = L.map('map').setView([companyLocation.lat, companyLocation.lng], 3);
+
+// @ts-ignore
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// @ts-ignore
+var marker = L.marker([companyLocation.lat, companyLocation.lng]).addTo(map);
+// @ts-ignore
+var marker = L.marker([userLocation.lat, userLocation.lng]).addTo(map);
