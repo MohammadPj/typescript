22d1db6c53ff1107eb85c21e30c1f7bbc8a2bd7b
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-20 16:25:01 +0430

    10-9 type Guards

--- 3-sort/src/index.ts
+++ 3-sort/src/index.ts
@@ -6,17 +6,26 @@ class Sorter {
   // }
 
   //  new syntax
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for(let j = 0; j < length - i - 1; j++) {
-        if(this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j]
-          this.collection[j] = this.collection[j + 1]
-          this.collection[j + 1] = temp
+
+        //  if collection is an Array of number
+        if(this.collection instanceof Array) {
+          if(this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j]
+            this.collection[j] = this.collection[j + 1]
+            this.collection[j + 1] = temp
+          }
+        }
+
+      //  if collection is a string
+        if (typeof this.collection === "string") {
+
         }
       }
     }
