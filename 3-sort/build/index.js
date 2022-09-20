5d2e150ba85ef87df4841ad57389d68c0ba10dca
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-20 16:37:00 +0430

    10-11 Extracting key logic

--- 3-sort/build/index.js
+++ 3-sort/build/index.js
@@ -1,33 +1,4 @@
 "use strict";
-class Sorter {
-    //  old syntax
-    // collection: number[];
-    // constructor(collection: number[]) {
-    //   this.collection = collection
-    // }
-    //  new syntax
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                //  if collection is an Array of number
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-                //  if collection is a string
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    }
-}
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
--- 3-sort/src/index.ts
+++ 3-sort/src/index.ts
@@ -1,36 +1,3 @@
-class Sorter {
-  //  old syntax
-  // collection: number[];
-  // constructor(collection: number[]) {
-  //   this.collection = collection
-  // }
-
-  //  new syntax
-  constructor(public collection: number[] | string) {}
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for(let j = 0; j < length - i - 1; j++) {
-
-        //  if collection is an Array of number
-        if(this.collection instanceof Array) {
-          if(this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j]
-            this.collection[j] = this.collection[j + 1]
-            this.collection[j + 1] = temp
-          }
-        }
-
-      //  if collection is a string
-        if (typeof this.collection === "string") {
-
-        }
-      }
-    }
-  }
-}
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort()
