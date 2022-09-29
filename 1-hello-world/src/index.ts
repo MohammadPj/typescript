73a04096b9278aa950f4cb416919bd57b3fff858
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-29 20:15:17 +0330

    26- type Alies

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,9 +1,14 @@
-let employee: { readonly id: number; name: string; isVIP?: boolean, retire: (date: Date) => void } = {
+type Employee = {
+  readonly id: number;
+  name: string;
+  isVIP?: boolean;
+  retire: (date: Date) => void;
+}
+
+let employee: Employee = {
   id: 1,
   name: "Mohammad",
   retire: (date: Date) => {
-    console.log("date", date)
-  }
+    console.log("date", date);
+  },
 };
-
-employee.id = 0
