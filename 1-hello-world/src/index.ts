497072b3d887af39ad1ef6b6881b75c1cc94e72f
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-29 15:17:15 +0330

    15- types and inferring

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,3 +1,7 @@
-let age: number = 20
-if(age < 50)
-  age += 10
+//  تایپ اسکریپت از طریق inferring خودش نوع داده ها رو مشخص میکنه
+//  انواع داده در جاواسکریپت number - string - boolean - null - undefined - object
+//  انواع داده در تایپ اسکریپت any - unknown - never - enum - tuple
+
+let sales = 123_456_789
+let course = "Typescript"
+let is_published = true
