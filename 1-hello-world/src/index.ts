02f27a975ac28ac91bcd5307421d7923485f59bd
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-29 17:37:19 +0330

    16- The Array Types

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,7 +1,2 @@
-//  تایپ اسکریپت از طریق inferring خودش نوع داده ها رو مشخص میکنه
-//  انواع داده در جاواسکریپت number - string - boolean - null - undefined - object
-//  انواع داده در تایپ اسکریپت any - unknown - never - enum - tuple
-
-let sales = 123_456_789
-let course = "Typescript"
-let is_published = true
+//  The Array Types
+let numbers: number[] = [1,2,3]
