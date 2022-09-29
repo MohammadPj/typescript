8938194bb6ca218bb6b69b4b4a0a9141178b11d2
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-29 18:31:32 +0330

    20- enum

--- 1-hello-world/dist/index.js
+++ 1-hello-world/dist/index.js
@@ -1,5 +1,10 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 2] = "Small";
+    Size[Size["Medium"] = 3] = "Medium";
+    Size[Size["Large"] = 4] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log("mySize", mySize);
 //# sourceMappingURL=index.js.map
--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,2 +1,7 @@
-//  The Tuple Type
-let users: [number, string] = [1, "mohammad"]
+// const small = 1
+// const medium = 2
+// const large = 3
+
+enum Size {Small = 2, Medium, Large}
+let mySize: Size = Size.Medium
+console.log("mySize", mySize)
