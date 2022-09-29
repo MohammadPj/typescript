1b4e058758263c486dceff159711f26b04fe9f0d
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-29 23:34:15 +0330

    31- Optional chaining

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,6 +1,18 @@
-function greet(name: string | null | undefined) {
-  if (name)
-    console.log("name", name)
-  else
-    console.log("Hola")
+type Customer = {
+  birthday?: Date
 }
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : {birthday: new Date()}
+}
+
+let customer = getCustomer(0)
+//  Optional property access operator
+console.log(customer?.birthday?.getFullYear())
+
+//  Optional element access operator
+//  customer?.[0]
+
+//  Optional call
+let log: any = null
+log?.(0)
