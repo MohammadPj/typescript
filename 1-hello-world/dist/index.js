37e8b4c166c4ba7d9d806faac7aa0cda8207a726
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-30 15:40:26 +0330

    43- private property in class

--- 1-hello-world/dist/index.js
+++ 1-hello-world/dist/index.js
@@ -1,17 +1,20 @@
 "use strict";
 class Account {
     constructor(id, owner, balance) {
-        this.balance = balance;
+        this._balance = balance;
         this.id = id;
         this.owner = owner;
     }
-    diposite(amount) {
+    getBalance() {
+        return this._balance;
+    }
+    deposit(amount) {
         if (amount < 0)
             throw new Error('invalid amount');
-        this.balance += amount;
+        this._balance += amount;
     }
 }
 let account = new Account(10, "mohammad", 0);
-account.diposite(100);
-console.log(account instanceof Account);
+account.deposit(100);
+console.log("balance", account.getBalance());
 //# sourceMappingURL=index.js.map
--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,23 +1,26 @@
 class Account {
   readonly id: number;
   owner: string;
-  balance: number
-  nickName?: string
+  private _balance: number
 
   constructor(id: number, owner: string, balance: number) {
-    this.balance = balance
+    this._balance = balance
     this.id = id
     this.owner = owner
   }
 
-  diposite(amount: number): void {
+  getBalance() {
+    return this._balance
+  }
+
+  deposit(amount: number): void {
     if (amount < 0)
       throw new Error('invalid amount')
-    this.balance += amount
+    this._balance += amount
   }
 }
 
 let account = new Account(10, "mohammad", 0)
 
-account.diposite(100)
-console.log(account instanceof Account)
+account.deposit(100)
+console.log("balance", account.getBalance())
