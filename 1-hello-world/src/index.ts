c761c356fe5fbcd4c2ac04c65e91ae541579bf2b
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-30 15:49:54 +0330

    44- Parameter Properties

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,26 +1,21 @@
 class Account {
-  readonly id: number;
-  owner: string;
-  private _balance: number
-
-  constructor(id: number, owner: string, balance: number) {
-    this._balance = balance
-    this.id = id
-    this.owner = owner
-  }
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
 
   getBalance() {
-    return this._balance
+    return this._balance;
   }
 
   deposit(amount: number): void {
-    if (amount < 0)
-      throw new Error('invalid amount')
-    this._balance += amount
+    if (amount < 0) throw new Error("invalid amount");
+    this._balance += amount;
   }
 }
 
-let account = new Account(10, "mohammad", 0)
+let account = new Account(10, "mohammad", 0);
 
-account.deposit(100)
-console.log("balance", account.getBalance())
+account.deposit(100);
+console.log("balance", account.getBalance());
