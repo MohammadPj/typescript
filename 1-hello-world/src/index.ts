58052afc57f774daa79c3cee37207e74adccb730
Mohammad <mohammadpoorjamal@gmail.com>
2022-09-30 12:50:02 +0330

    35- The never types

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,7 +1,13 @@
-function render(document: unknown) {
-  // Narrowing
-  if(typeof document === "string") {
-    document.toLowerCase()
-  }
-  document.toLowerCase()
+// function processEvents(): never {
+//   while (true) {
+//     //  read a message from a queue
+//   }
+// }
+
+function reject(message: string): never {
+  throw new Error(message)
 }
+
+reject("we have an Error")
+// processEvents()
+console.log("salam")
