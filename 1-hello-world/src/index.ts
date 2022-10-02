f67b28cccf0fe859087f27d4d3c1c205653fd1f4
Mohammad <mohammadpoorjamal@gmail.com>
2022-10-02 17:12:45 +0330

    51- protected vs private class

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,45 +1,16 @@
 class Person {
-  constructor(public firstName: string, public lastName: string) {
+  private walk() {
+    console.log("walk")
   }
 
-  get fullName() {
-    return  this.firstName + " " + this.lastName
-  }
-
-  walk() {
-    console.log("walking")
+  protected talk() {
+    console.log("talk")
   }
 }
 
 class Student extends Person {
-  constructor(public studentId: number, firstName: string, lastName: string) {
-    super(firstName, lastName);
-  }
-
-  takeTest() {
-    console.log("Taking a Test")
+  doSomthing() {
+    this.walk()
+    this.talk()
   }
 }
-
-class Teacher extends Person {
-  override get fullName() {
-    return 'Professor ' + super.fullName
-  }
-}
-
-class Principle extends Person {
-  override get fullName(): string {
-    return "principle " + super.fullName;
-  }
-}
-
-function printName(people: Person[]) {
-  for (let person of people)
-    console.log(person.fullName)
-}
-
-printName([
-  new Student(1, "Mohammad", "Poorjamal"),
-  new Teacher("Mosh", "Hamedani"),
-  new Principle("john", "smith")
-])
