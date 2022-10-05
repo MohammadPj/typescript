692f4f96c6edd00d738a1037fdb89d33b64c6d3a
Mohammad <mohammadpoorjamal@gmail.com>
2022-10-05 23:35:55 +0330

    59- Generic Classes

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,35 +1,6 @@
-interface Calendar {
-  name: string;
-  addEvents(): void;
-  removeEvent(): void;
-}
-
-interface CloudCalendar extends Calendar {
-  sync(): void;
-}
-
-class GoogleCalendar implements CloudCalendar {
-  constructor(public name: string) {}
-
-  addEvents(): void {}
-
-  removeEvent(): void {}
-
-  sync(): void {}
-}
-
-let creatCalendar = (calendar: Calendar): void => {
-  console.log("creating ", calendar)
-}
-
-let calendar: Calendar = {
-  name: "google",
-  addEvents() {
-    console.log("add events")
-  },
-  removeEvent() {
-    console.log("remove events")
+class KeyValuePair<K, V> {
+  constructor(public key: K, public value: V) {
   }
 }
 
-creatCalendar(calendar);
+let pair = new KeyValuePair<string, number>('a', 10)
