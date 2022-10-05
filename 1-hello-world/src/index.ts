9d9054c5ee2f468cc6576e4ad417df66aee67a3f
Mohammad <mohammadpoorjamal@gmail.com>
2022-10-06 00:03:35 +0330

    61- Generic interface

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,12 +1,21 @@
-class KeyValuePair<K, V> {
-  constructor(public key: K, public value: V) {
-  }
-
-  wrapInArray<T>(value: T) {
-    return [value]
-  }
+interface Result<T> {
+  data: T | null;
+  error: string | null;
+}
+interface User {
+  username: string;
+}
+interface Product {
+  title: string;
 }
 
-let pair = new KeyValuePair<string, number>('a', 10)
+function fetch<T>(url: string): Result<T> {
+  console.log(url);
+  return { data: null, error: null };
+}
 
-let numbers = pair.wrapInArray<number>(20)
+let userResult = fetch<User>("user url");
+let productResult = fetch<Product>("product url");
+
+console.log(userResult.data?.username);
+console.log(productResult.data?.title);
