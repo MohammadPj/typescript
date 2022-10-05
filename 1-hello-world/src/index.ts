97df32ea58cf2e573e2a9ebeb1a0e58b31c2d3c9
Mohammad <mohammadpoorjamal@gmail.com>
2022-10-05 23:41:32 +0330

    60- Generic Functions

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,6 +1,12 @@
 class KeyValuePair<K, V> {
   constructor(public key: K, public value: V) {
   }
+
+  wrapInArray<T>(value: T) {
+    return [value]
+  }
 }
 
 let pair = new KeyValuePair<string, number>('a', 10)
+
+let numbers = pair.wrapInArray<number>(20)
