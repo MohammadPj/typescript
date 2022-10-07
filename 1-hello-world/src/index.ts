90988e179ebdf3fcb4698e7c7bf0bfdba37fd6f0
Mohammad <mohammadpoorjamal@gmail.com>
2022-10-07 17:09:37 +0330

    76- Parameter Decorators

--- 1-hello-world/src/index.ts
+++ 1-hello-world/src/index.ts
@@ -1,30 +1,19 @@
-function MinLength(length: number) {
-  return (target: any, propertyName: string) => {
-    let value : string;
-
-    const descriptor: PropertyDescriptor = {
-      get() {return value},
-      set(newValue: string) {
-        if (newValue.length < length)
-          throw new Error(`Property should be at least ${length} characters long.`)
-        value = newValue
-      }
-    }
-
-    Object.defineProperty(target, propertyName, descriptor)
-  }
+type WatchedParameter = {
+  methodName: string,
+  parameterIndex: number
 }
 
+const watchedParameters: WatchedParameter[] = []
 
-class User {
-  @MinLength(4)
-  password: string;
-
-  constructor(password: string) {
-    this.password = password
-  }
+function Watch (target: any, methodName: string, parameterIndex: number) {
+  watchedParameters.push({
+    methodName,
+    parameterIndex
+  })
 }
 
-let user = new User("123")
+class vehicle {
+  move(@Watch speed: number) {}
+}
 
-console.log(user.password)
+console.log(watchedParameters)
